b17c0e59a76b4e49a3ea939a3f57c2f2adc30f98
Olwethu Theo Nyondo <32870672+Owzaa@users.noreply.github.com>
2019-07-29 11:02:30 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UrnBMS.Startup))]
+namespace UrnBMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
