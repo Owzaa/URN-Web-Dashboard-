a0eaf3da6d17d4aae6417e561b4340f3f93e9ec7
Olwethu Theo Nyondo <32870672+Owzaa@users.noreply.github.com>
2019-07-29 10:59:27 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bytes Inventory Management")]
+[assembly: AssemblyDescription("Manges inventory, workflow, employees and documentation.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tweakhouse Multimedia (Pty) Ltd & MSDC Inc (Pty) Ltd")]
+[assembly: AssemblyProduct("Bytes Inventory Management")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("89602938-0f96-42d3-af38-87cee62f9cac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-ZA")]
+
